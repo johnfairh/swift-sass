63392d26403974341c80b6f47d529c68118b3e80
John Fairhurst <johnfairh@gmail.com>
2021-03-12 15:30:04 +0000

    Doc comments

--- Sources/SassLibSass/Compiler.swift
+++ Sources/SassLibSass/Compiler.swift
@@ -8,11 +8,19 @@
 @_exported import Sass
 import struct Foundation.URL
 
-public final class Compiler {
+/// A Sass compiler interface using LibSass.
+///
+public struct Compiler {
     private let messageStyle: CompilerMessageStyle
     private let globalImporters: [ImportResolver]
     private let globalFunctions: SassFunctionMap
 
+    /// Set up a new instance of the compiler.
+    ///
+    /// - parameter messageStyle: Style for diagnostic message descriptions.  Default `.plain`.
+    /// - parameter importers: Rules for resolving `@import` that cannot be satisfied relative to
+    ///   the source file's path, used for all this compiler's compilations.
+    /// - parameter functions: Sass functions available to all this compiler's compilations.
     public init(messageStyle: CompilerMessageStyle = .plain,
                 importers: [ImportResolver] = [],
                 functions: SassFunctionMap = [:]) {
@@ -26,6 +34,19 @@ public final class Compiler {
         LibSass.version
     }
 
+    /// Compile to CSS from a stylesheet file.
+    ///
+    /// - parameters:
+    ///   - fileURL: The URL of the file to compile.  The file extension determines the
+    ///     expected syntax of the contents, so it must be css/scss/sass.
+    ///   - outputStyle: How to format the produced CSS.  Default `.nested`.
+    ///   - createSourceMap: Create a JSON source map for the CSS.  Default `false`.
+    ///   - importers: Rules for resolving `@import` etc. for this compilation, used in order after
+    ///     `sourceFileURL`'s directory and any set globally..  Default none.
+    ///   - functions: Functions for this compilation, overriding any with the same name previously
+    ///     set globally. Default none.
+    /// - throws: `CompilerError` if the stylesheet can't be compiled, for example a syntax error.
+    /// - returns: `CompilerResults` with CSS and optional source map.
     public func compile(fileURL: URL,
                         outputStyle: CssStyle = .nested,
                         createSourceMap: Bool = false,
@@ -37,6 +58,22 @@ public final class Compiler {
                            importers: importers, functions: functions)
     }
 
+    /// Compile to CSS from an inline stylesheet.
+    ///
+    /// - parameters:
+    ///   - string: The stylesheet text to compile.
+    ///   - syntax: The syntax of `text`, default `.scss`.
+    ///   - fileURL: The absolute URL to associate with `string`, from where it was loaded.
+    ///     Default `nil` meaning unknown.  LibSass substitutes something like `stream://stdin`
+    ///     where necessary.
+    ///   - outputStyle: How to format the produced CSS.  Default `.nested`.
+    ///   - createSourceMap: Create a JSON source map for the CSS.  Default `false`.
+    ///   - importers: Rules for resolving `@import` etc. for this compilation, used in order after
+    ///     any set globally.  Default none.
+    ///   - functions: Functions for this compilation, overriding any with the same name previously
+    ///     set globally.  Default none.
+    /// - throws: `CompilerError` if the stylesheet can't be compiled, for example a syntax error.
+    /// - returns: `CompilerResults` with CSS and optional source map.
     public func compile(string: String, syntax: Syntax = .scss, fileURL: URL? = nil,
                         outputStyle: CssStyle = .nested,
                         createSourceMap: Bool = false,
