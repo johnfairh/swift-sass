8ad585bf42981380bc569230069d8f5ee81af4bf
John Fairhurst <johnfairh@gmail.com>
2021-05-26 14:02:25 +0100

    CSS + Source Map URL manipulation (#12)

--- Sources/Cli/main.swift
+++ Sources/Cli/main.swift
@@ -13,25 +13,48 @@ import DartSass
 import Foundation
 
 let args = ProcessInfo.processInfo.arguments
-guard args.count == 3 else {
-    fputs("Syntax: ssassc <input file> <output file>\n", stderr)
+guard args.count == 5 else {
+    fputs("Syntax: ssassc dart|libsass <input file> <output css file> <output srcmap file>\n", stderr)
     exit(1)
 }
 
-do {
-    let input = URL(fileURLWithPath: args[1])
-    let output = URL(fileURLWithPath: args[2])
-
-    let compiler = try Compiler(eventLoopGroupProvider: .createNew,
-                                messageStyle: .terminalColored)
+func compileWithDartSass(input: URL) throws -> CompilerResults {
+    let compiler = try DartSass.Compiler(eventLoopGroupProvider: .createNew,
+                                         messageStyle: .terminalColored)
     defer { try? compiler.syncShutdownGracefully() }
 
-    let results = try compiler.compile(fileURL: input)
+    return try compiler.compile(fileURL: input)
+}
+
+func compileWithLibSass(input: URL) throws -> CompilerResults {
+    preconditionFailure()
+//    let compiler = LibSass.Compiler(messageStyle: .terminalColored)
+//    return try compiler.compile(fileURL: input)
+}
+
+let compilers = ["dart": compileWithDartSass,
+                 "libsass": compileWithLibSass]
+
+
+do {
+    let compilerName = args[1]
+    let input = URL(fileURLWithPath: args[2])
+    let outputCss = URL(fileURLWithPath: args[3])
+    let outputSrcMap = URL(fileURLWithPath: args[4])
+
+    guard let compiler = compilers[compilerName] else {
+        fputs("Unknown compiler '\(compilerName)' - should be 'dart' or 'libsass'\n", stderr)
+        exit(3)
+    }
+
+    let results = try compiler(input)
+        .withFileLocations(cssFileURL: outputCss, sourceMapFileURL: outputSrcMap)
 
     results.messages.forEach {
         print($0)
     }
-    try results.css.write(to: output, atomically: false, encoding: .utf8)
+    try results.css.write(to: outputCss, atomically: false, encoding: .utf8)
+    try results.sourceMap?.write(to: outputSrcMap, atomically: false, encoding: .utf8)
 } catch {
     fputs("Error: \(error)\n", stderr)
     exit(2)
--- Sources/Sass/CompilerResults.swift
+++ Sources/Sass/CompilerResults.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+@_implementationOnly import SourceMapper
 
 /// The output from a successful compilation.
 public struct CompilerResults {
@@ -24,5 +25,107 @@ public struct CompilerResults {
         self.css = css
         self.sourceMap = sourceMap
         self.messages = messages
+    }
+
+    // MARK: Source Map URL Control
+
+    /// How to reference source map resources from CSS.
+    ///
+    /// There are two references here: the CSS pointing at the source map, and the source map
+    /// pointing at the original sources.  The Sass compiler uses absolute (typically `file:`) URLs
+    /// for original sources.  This can be modified and the CSS -> source map reference added using
+    /// `CompilerResults.withFileLocations(...)`.
+    ///
+    /// If you plan to ship the source map (so it is accessed through `http:` URLs by a browser)
+    /// then you need to use one of the `relative` options to avoid `file:` URLs that won't
+    /// make sense to clients.
+    ///
+    public enum URLStyle {
+        /// Use absolute (`file:` for filesystem files) URLs everywhere.
+        case allAbsolute
+
+        /// Use absolute URLs in the source map to refer to the original sources, but use a relative
+        /// URL to refer to the source map from the CSS.
+        case sourcesAbsolute
+
+        /// Use relative URLs everywhere possible.  For example if an original source is referenced
+        /// with an `https:` or `data:` URL then that version is preserved.
+        case relative
+
+        /// Like `relative`, but also set the `sourceRoot` source map field to add extra path
+        /// segments between the source map and the original sources.
+        case relativeSourceRoot(String)
+    }
+
+    /// Error indicating there is no source map present when one is required.  :nodoc:
+    public struct NoSourceMapError: Swift.Error {}
+
+    /// Add filenames to the CSS and its source map.
+    ///
+    /// The CSS and source map come out of the Sass compiler without references to each other.  Sources
+    /// are referenced from the source map using absolute URLs.  This routine updates various metadata to
+    /// make the source map load correctly in a browser.
+    ///
+    /// This routine does not write any files.
+    ///
+    /// - parameter cssFileURL: The file URL for the CSS.  This is used to update the source map
+    ///   with the name of the file it's describing and, if `style` requires it, calculation of relative paths.
+    /// - parameter sourceMapFileURL: The file URL for the source map.  This is used to generate the
+    ///   *sourceMappingURL* comment in the CSS.
+    /// - parameter style: How to reference the source map from the CSS and the sources from the
+    ///   source map.  See `URLStyle`; the default is `.relative`.
+    /// - throws: If the `CompilerResults` does not have a source map, or if JSON encoding/
+    ///   decoding goes wrong.
+    /// - returns: A new `CompilerResults` with updated `css` and `sourceMap` fields.  The
+    ///   `messages` field is copied over unchanged.
+    public func withFileLocations(cssFileURL: URL, sourceMapFileURL: URL, style: URLStyle = .relative) throws -> CompilerResults {
+        guard let sourceMapString = self.sourceMap else {
+            throw NoSourceMapError()
+        }
+
+        let sourceMap = try SourceMap(string: sourceMapString)
+        sourceMap.file = cssFileURL.lastPathComponent
+
+        let actualSourceMapURL: String
+
+        switch style {
+        case .allAbsolute:
+            actualSourceMapURL = sourceMapFileURL.absoluteString
+
+        case .relativeSourceRoot(let sourceRoot):
+            sourceMap.sourceRoot = sourceRoot
+            fallthrough
+
+        case .relative:
+            sourceMap.sources = sourceMap.sources.map {
+                .init(url: URL(string: $0.url)!.asRelativeURL(from: cssFileURL), content: $0.content)
+            }
+            fallthrough
+
+        case .sourcesAbsolute:
+            actualSourceMapURL = sourceMapFileURL.asRelativeURL(from: cssFileURL)
+        }
+
+        return CompilerResults(
+            css: css + "\n/*# sourceMappingURL=\(actualSourceMapURL) */\n",
+            sourceMap: try sourceMap.encodeString(continueOnError: true),
+            messages: messages)
+    }
+}
+
+// internal for test
+extension URL {
+    /// Rework a file URL relative to some other file - only useful if both are `file:`
+    func asRelativeURL(from: URL) -> String {
+        guard isFileURL && from.isFileURL else {
+            return absoluteString
+        }
+        let myComponents = pathComponents
+        let fromComponents = from.pathComponents
+        let pieces = zip(myComponents, fromComponents)
+        let shared = pieces.prefix(while: { $0 == $1 }).count
+        let comps = Array(repeating: "..", count: fromComponents.count - shared - 1) +
+            myComponents.suffix(myComponents.count - shared)
+        return comps.joined(separator: "/")
     }
 }
